dc8cc84fd07de3a9116a2262b366ac737fef522b
Nick Berta <nicholas.berta1@t-mobile.com>
2022-07-26 13:03:51 -0500

    Added base route '/' for path

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, world!);
+});
+
 app.get("/health", (req, res) => {
   res.status(200).send("Don't panic.");
 });
