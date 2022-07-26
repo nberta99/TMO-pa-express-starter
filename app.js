72a08913a841caa86da1e2c99fa6ca4ef9c2bb04
Nick Berta <nicholas.berta1@t-mobile.com>
2022-07-26 13:05:52 -0500

    fixed syntax error

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 // app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello, world!);
+  res.status(200).send("Hello, world!");
 });
 
 app.get("/health", (req, res) => {
